e0981815705b8578e87ba99819045e9d08968471
croschel <croschel000@gmail.com>
2022-03-21 18:09:40 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '~/assets/logo.svg';
+import { Container, Content } from './styles';
+
+export const Header: React.FC = () => (
+  <Container>
+    <Content>
+
+      <img src={logo} alt="dt money" />
+      <button type="button">Nova transação</button>
+    </Content>
+  </Container>
+);
